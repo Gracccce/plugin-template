ce4ec730a5b32183963914fca01a5f08fa7b9056
GitHub Action <action@github.com>
2020-12-02 11:27:25 +0000

    Template cleanup

--- src/main/kotlin/com/github/gracccce/plugintemplate/listeners/MyProjectManagerListener.kt
+++ src/main/kotlin/com/github/gracccce/plugintemplate/listeners/MyProjectManagerListener.kt
@@ -1,9 +1,9 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.gracccce.plugintemplate.listeners
 
 import com.intellij.openapi.components.service
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.ProjectManagerListener
-import org.jetbrains.plugins.template.services.MyProjectService
+import com.github.gracccce.plugintemplate.services.MyProjectService
 
 internal class MyProjectManagerListener : ProjectManagerListener {
 
--- src/main/kotlin/com/github/gracccce/plugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/gracccce/plugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.gracccce.plugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.gracccce.plugintemplate.MyBundle
 
 class MyApplicationService {
 
